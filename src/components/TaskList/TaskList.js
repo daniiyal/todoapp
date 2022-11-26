b6aaf6b4a6826f088d3a37c6fd87359daba511c9
m_daniyal <danik@magomedow@mail.ru>
2022-11-26 12:40:07 +0300

    сделал todolist

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './TaskList.css'
+import Task from '../Task/Task';
+
+class TasksList extends React.Component{
+
+    render(){
+        return (
+            <div className='tasks-list-container'>
+                <div className='tasks-list'>                  
+                    { this.props.tasks.map(task => <Task key={task.key} text={task.text} id={task.id} removeTask={this.props.removeTask}/>) }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TasksList;
